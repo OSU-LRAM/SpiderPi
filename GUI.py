477c7d66077a96ae1313ebe2c4cafc5da93202c3
Ryan Quick <51896082+RyanQuick@users.noreply.github.com>
2021-07-05 17:22:01 -0700

    Test worked, just have problem importing play_audio.py

--- GUI.py
+++ GUI.py
@@ -9,7 +9,7 @@ import time
 import board
 import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 import os
-#from play_audio.py import play_audio
+from play_audio.py import play_audio
 
 
 # Modify this if you have a different sized Character LCD
